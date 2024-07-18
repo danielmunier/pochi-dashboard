975853536cae0dd840cace9baf4ca221e9baf978
Daniel <idanielmunier@gmail.com>
2024-07-18 17:50:49 -0300

    config route

--- src/app/api/guilds/[id]/config/route.ts
+++ src/app/api/guilds/[id]/config/route.ts
@@ -3,51 +3,41 @@ import axios from "axios";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-
-export async function GET(request: NextRequest, { params }: { params: { id: string } }, response: NextResponse) {
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
     try {
-        const { id } = params
-        const authorization = headers().get('Cookie')
+        const { id } = params;
+        const authorization = headers().get('Cookie');
 
         const { data: guildData } = await axios(`${process.env.API_URL}/api/guilds/${id}/config`, {
             headers: { authorization }
-        })
+        });
 
-        return NextResponse.json(guildData)
+        return NextResponse.json(guildData);
     } catch (error: any) {
-        console.log(error)
-        return NextResponse.json({ error: error.message }, {status: 500})
+        console.log(error);
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
 
-
-
-export async function POST(request: NextRequest, { params }: { params: { id: string } }, response: NextResponse) {
+export async function POST(request: NextRequest, { params }: { params: { id: string } }) {
     try {
-        const data = await request.json()
-        const { id } = params
-        const authorization = headers()
+        const data = await request.json();
+        const { id } = params;
+        const authorization = headers();
 
-        const isValidGuild = await fetchValidGuild(id, authorization)
+        const isValidGuild = await fetchValidGuild(id, authorization);
         if (isValidGuild) {
-            const { id } = params
-            const authorization = headers().get('Cookie')
-
             const { data: guildData } = await axios.post(`${process.env.API_URL}/api/guilds/${id}/config`, {
                 headers: { authorization },
                 data
-            }
-            )
+            });
 
-            return NextResponse.json(guildData)
+            return NextResponse.json(guildData);
         }
 
-        //return response.status !== 200 ? NextResponse.next() : NextResponse.redirect(new URL("/", req.url))
-
-
+        return NextResponse.json({ error: 'Invalid guild' }, { status: 400 });
     } catch (error: any) {
-        console.log(error)
-        return { error: error.message }
+        console.log(error);
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
-
