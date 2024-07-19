50ad02b27383e2cf858cfd6842ba3d8a1731b64f
Daniel <idanielmunier@gmail.com>
2024-07-19 13:55:59 -0300

    debug in production

--- src/app/api/auth/discord/route.ts
+++ src/app/api/auth/discord/route.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
+import { Console } from "console";
 
 import { NextRequest, NextResponse } from "next/server";
 
 // Método GET para a rota específica
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
     try {
+        console.log("Tentativa de redirecionamento para a rota")
         return NextResponse.redirect(`${process.env.API_URL}/api/auth/discord/redirect`);
 
     } catch (error: any) {
