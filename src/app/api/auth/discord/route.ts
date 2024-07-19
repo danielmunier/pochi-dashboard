ab5b749c74003757df0db6d1d637f447dce436b1
Daniel <idanielmunier@gmail.com>
2024-07-19 13:50:29 -0300

    status not needed in redirect

--- src/app/api/auth/discord/route.ts
+++ src/app/api/auth/discord/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 // Método GET para a rota específica
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
     try {
-        return NextResponse.redirect(`${process.env.API_URL}/api/auth/discord/redirect`, {status: 200});
+        return NextResponse.redirect(`${process.env.API_URL}/api/auth/discord/redirect`);
 
     } catch (error: any) {
         // Retorne um erro como JSON
