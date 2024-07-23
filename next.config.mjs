f410bed092ba34c121243043c3564b3449a8c0f2
Daniel <idanielmunier@gmail.com>
2024-07-23 01:11:07 -0300

    removed path

--- next.config.mjs
+++ next.config.mjs
@@ -14,10 +14,9 @@ const nextConfig = {
   reactStrictMode: false,
   typescript: {
     ignoreBuildErrors: true
-  },
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    return config;}
-};
+  }
+
+}
+ 
 
 export default nextConfig;
