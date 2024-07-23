7df310e1dadbeec18bf8ee20ae8a7f39a9a79221
Daniel <idanielmunier@gmail.com>
2024-07-23 00:34:25 -0300

    vercel test

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -4,7 +4,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 export async function GET(req: NextRequest, response: NextResponse) {
-    const session = await auth()
-    console.log(session)
+
     return new Response("Hello, world!");
 }
