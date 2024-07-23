3eb68cc34943c0020397763ab1c5a0d8102df734
Daniel <idanielmunier@gmail.com>
2024-07-23 01:07:50 -0300

    fix import

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,10 @@ const nextConfig = {
   reactStrictMode: false,
   typescript: {
     ignoreBuildErrors: true
-  }
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;}
 };
 
 export default nextConfig;
