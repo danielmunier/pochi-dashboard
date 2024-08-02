8a58f9717180c2d3af5112898d9bea7ad310cb39
Daniel <idanielmunier@gmail.com>
2024-08-01 23:30:45 -0300

    README.md screenshots

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+
+# Screenshots
+## Página inicial
+![Homepage](https://i.imgur.com/RiTg9xg.png)
+## Página de configuração atual
+![Configuration](https://i.imgur.com/8QmgCrx.png)
+
+
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- src/app/components/guilds/GuildMenuItem.tsx
+++ src/app/components/guilds/GuildMenuItem.tsx
@@ -18,8 +18,8 @@ export const GuildMenuItem: FC<GuildMenuItemProps> = ({ guild }) => {
   return (
    
       <Link href={`/dashboard/${guild.id}`}>
-        <div className="border border-gray-700 rounded-lg p-4 mb-4 text-white cursor-pointer flex items-center space-x-4 hover:bg-gray-700 transition duration-300">
-          <img src={iconUrl} alt={`${guild.name} icon`} className="w-12 h-12 rounded-full" />
+        <div className="border border-gray-700 rounded-lg p-4 mb-4 text-white cursor-pointer flex items-center space-x-4 hover:bg-gray-700 transition duration-200">
+          <img src={iconUrl} alt={`${guild.name} icon`} className="w-14 h-14 rounded-full" />
           <div>
             <h2 className="text-xl font-bold">{guild.name}</h2>
             <p className="text-gray-400">{guild.description || "No description available"}</p>
