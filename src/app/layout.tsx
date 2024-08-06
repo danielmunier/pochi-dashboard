3ce91141b4586d78aab784c681771c1090b0c9a3
Daniel <idanielmunier@gmail.com>
2024-08-06 02:56:22 -0300

    icon in og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   title: "Pochi",
   description: "Olá, me chamo Pochi! Sou um bot vários propósitos e funções para o Discord. Me adicione em seu servidor!",
   keywords: ["Pochi", "Bot Discord", ""],
-  icons: ["https://cdn.discordapp.com/icons/1041140810881699860/5f5e3eafb46342d45703cb973e17934f.png"],
+  icons: [new URL("https://cdn.discordapp.com/icons/1041140810881699860/5f5e3eafb46342d45703cb973e17934f.png")],
  
   
 };
