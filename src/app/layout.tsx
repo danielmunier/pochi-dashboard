c4cbbadc55646a0dbd243a67eed40a9dc28ee9a2
Daniel <idanielmunier@gmail.com>
2024-08-06 02:34:24 -0300

    testando og tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,20 @@
 import type { Metadata } from "next";
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
 import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
 
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
-import Icon from "@/app/icon.png"
+import Icon from "@/app/icon.png"; // Import the icon for the favicon
 import NextAuthSessionProvider from "@/providers/sessionProvider";
 import Sidebar from "./components/misc/sidebar/nav";
 import { ThemeProvider } from "@/providers/theme-provider";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pochi",
   description: "Olá, me chamo Pochi! Sou um bot vários propósitos e funções para o Discord. Me adicione em seu servidor!",
-
-
-
 };
 
 export default async function RootLayout({
@@ -26,13 +22,20 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
-
   return (
-
     <html lang="en">
       <head>
         <link rel="icon" href={`${Icon}`} type="image/png" />
+        <meta property="og:title" content="Pochi" />
+        <meta property="og:description" content="Olá, me chamo Pochi! Sou um bot vários propósitos e funções para o Discord. Me adicione em seu servidor!" />
+        <meta property="og:image" content={`${Icon}`} />
+        <meta property="og:url" content="https://pochi-dashboard.vercel.app/" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Pochi" />
+        <meta name="twitter:description" content="Olá, me chamo Pochi! Sou um bot vários propósitos e funções para o Discord. Me adicione em seu servidor!" />
+        <meta name="twitter:image" content={`${Icon}`} />
       </head>
       <body className={inter.className}>
         <ThemeProvider defaultTheme="light" storageKey="pochi-theme">
@@ -45,6 +48,5 @@ export default async function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-
   );
 }
